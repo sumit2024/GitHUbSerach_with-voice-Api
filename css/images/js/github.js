1a4402f4c2f090c28f6dc895583e78ef0d2fcf43
sumit <you@example.com>
2020-09-17 11:36:51 +0530

    first commit

---
+++ css/images/js/github.js
@@ -0,0 +1,209 @@
+// let search = document.querySelector("#search");
+// let template = document.querySelector("#template");
+
+// //Keyboard Search
+// search.addEventListener("keyup", (e) => {
+//   let searchText = e.target.value;
+//   SearchGitHubProfiles(searchText);
+// });
+// // /*   serach by voice */
+// let searchByVoice = document.querySelector('#speachIcon')
+// searchByVoice.addEventListener('click',(e) =>{
+//     window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+//     let recognition  = new SpeechRecognition();
+//     recognition.interimResults = true;
+//     recognition.addEventListener('result', e =>{
+//         let transcript = [...e.results]
+//         .map((result) => result[0])
+//         .map((result) => result.transcript)
+//         .join("")
+//         .replace(/\s/g,"")
+
+//         search.value = transcript
+//        let searchText = transcript;
+//        SearchGitHUbProfiles(searchText)
+//     })
+//     recognition.start();
+// })
+// // /-----github api ------/
+
+// function SearchGitHubProfiles(searchText) {
+//   let Client_Id = "753e889794fc17669cf0";
+//   let Client_Secret = "57ca0b1c1e5ccc68f42c1a005c8662aad6d218bb";
+//   let BASE_URL = `https://api.github.com/users/${searchText}?client_id=${Client_Id}&client_secret=${Client_Secret}`;
+//   //basic github auth
+
+
+  
+//   window
+//     .fetch(BASE_URL)
+//     .then((data) => {
+//       //ReadableStream
+//       data
+//         .json()
+//         .then((users) => {
+//           if (users.message === "Not Found") {
+//             template.innerHTML = `<h1 style="color:red">No Github Profile Found</h1>`;
+//           } else {
+//             let output = "";
+//             output += `
+//               <section id="ProfileBlock">
+//                 <article>
+//                   <div class="leftBlock">
+//                      <figure>
+//                      <a href="${users.html_url}" traget="_blank>
+//                      <img src="${users.avatar_url}" alt="${users.login}" />
+//                      </a>
+//                      </figure>
+//                      <h4> <a href="${users.name}" target="_blank"></a></h4>
+//                      <h5>${users.login}</h5>
+//                      <h5>${users.bio}</h5>
+                   
+//                      <h5>${users.company}</h5>
+//                      <h5>${users.location}</h5>
+//                   </div>
+//                   <div class="rightBlock">
+//                   <div> 
+//                   <span>Public repos: <span class="innerspan">${users.public_repos}</span></span>
+//                   <span>Public gists: <span class="innerspan">${users.public_gists}</span></span>
+//                   <span>Public followers: <span class="innerspan">${users.public_followers}</span></span>
+//                   <span>Public following: <span class="innerspan">${users.public_following}</span></span>
+                  
+//                   </div>
+//                     <h1>right Block</h1>
+//                   </div>
+//                 </article>
+//               </section>
+//             `;
+//             template.innerHTML = output;
+//           }
+//         })
+//         .catch((err) => console.log(err));
+//     })
+//     .catch((err) => console.log(err));
+// }
+
+
+
+
+
+
+let search = document.querySelector("#search");
+let template = document.querySelector("#template");
+
+//Keyboard Search
+search.addEventListener("keyup", (e) => {
+  let searchText = e.target.value;
+  SearchGitHubProfiles(searchText);
+});
+
+/*-----search by Voice ----------*/
+let SearchByVoice = document.querySelector("#speechIcon");
+SearchByVoice.addEventListener("click", (e) => {
+  window.SpeechRecognition =
+    window.SpeechRecognition || window.webkitSpeechRecognition;
+  let recognition = new SpeechRecognition();
+  recognition.interimResults = true;
+
+  recognition.addEventListener("result", (e) => {
+    let transcript = [...e.results]
+      .map((result) => result[0])
+      .map((result) => result.transcript)
+      .join("")
+      .replace(/\s/g, "");
+
+    search.value = transcript;
+    let searchText = transcript;
+    SearchGitHubProfiles(searchText);
+  });
+
+  recognition.start();
+});
+
+/*-----github api ------*/
+
+function SearchGitHubProfiles(searchText) {
+  let Client_Id = "753e889794fc17669cf0";
+  let Client_Secret = "57ca0b1c1e5ccc68f42c1a005c8662aad6d218bb";
+  
+  let BASE_URL = `https://api.github.com/users/${searchText}?client_id=${Client_Id}&client_secret=${Client_Secret}`;
+  //basic github auth
+  let REPO_URL = `https://api.github.com/users/${searchText}/repos?client_id=${Client_Id}&client_secret=${Client_Secret}`;
+
+  //Github users
+  window
+    .fetch(BASE_URL)
+    .then((data) => {
+      //ReadableStream
+
+      data
+        .json()
+        .then((users) => {
+          if (users.message === "Not Found") {
+            template.innerHTML = `<h1 style="color:red">No Github Profile Found</h1>`;
+          } else {
+            let output = "";
+            output += `
+              <section id="ProfileBlock">
+                <article>
+                  <div class="leftBlock">
+                     <figure>
+                     <a href="${users.html_url}" target="_blank"> 
+                     <img src="${users.avatar_url}" alt="${users.login}" />
+                     </a>
+                     </figure>
+                     <h4><a href="${users.html_url}" target="_blank">${users.name}</a></h4>
+                     <h5>${users.login}</h5>
+                     <h5>${users.bio}</h5>
+                   
+                     <h5>${users.company}</h5>
+                     <h5>${users.location}</h5>
+                  </div>
+                  <div class="rightBlock">
+                    <div id="usersRepo">
+                      <span>Repositories : <span class="innerSpan">${users.public_repos}</span></span>
+                      <span>Public gists : <span class="innerSpan">${users.public_gists}</span></span>
+                      <span>Followers :<span class="innerSpan">${users.followers}</span></span>
+                      <span>Following : <span class="innerSpan">${users.following}</span></span>
+                    </div>
+                    <div id="repoBlock"></div>
+                  </div>
+                </article>
+              </section>
+            `;
+            template.innerHTML = output;
+          }
+        })
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err));
+
+  //Github Public Repository
+  window
+    .fetch(REPO_URL)
+    .then((data) => {
+      data
+        .json()
+        .then((repos) => {
+          let output_repos = [];
+          //for of loop
+
+          for (let repo of repos) {
+            console.log(repo);
+            output_repos += `
+              <main>
+                <h4><a href="${repo.html_url}" target="_blank">
+                ${repo.name}</a>
+                </h4>
+                <p>${repo.description}</p>
+                <p>${new Date(repo.created_at).toLocaleDateString()}</p>
+              </main>
+            `;
+          }
+          document.getElementById("repoBlock").innerHTML = output_repos;
+        })
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err));
+  //ending Github Public Repository
+}
